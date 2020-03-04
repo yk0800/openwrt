9d28038f4aa9d6f489bced18c008c20bd8158c26
Lienol <lawlienol@gmail.com>
2020-03-04 23:50:00 +0800

    unblockmusic : sync update
    unblockmusic: sync lean's package
    
    luci-app-unblockmusic: resize and update
    
    luci-app-unblockmusic: add wget depend
    
    UnblockNeteaseMusic:may can fix memory overflow
    
    luci-app-unblockmusic: add block NeteaseCloudMusic spec ip
    
    luci-app-unblockmusic: fix hijack
    
    luci-app-unblockmusic: fix hotplug.d script
    
    luci-app-unblockmusic: fix update core in low mem (=<128M) router
    
    luci-app-unblockneteasemusic-go: add go version unblockneteasemusic for NO-FPU dev
    
    luci-app-unblockneteasemusic-mini: add package (#3218)
    
    luci-app-unblockneteasemusic-mini: change name
    
    luci-app-unblockneteasemusic-mini: fix typo error (#3231)
    
    luci-app-unblockmusic: enable FLAC unlocked
    
    UnblockNeteaseMusic: bump to 0.24.1 (flac support)
    
    luci-app-unblockneteasemusic-mini: add flac server
    
    luci-app-unblockneteasemusic-mini: fix typo
    
    Calling the restart code directly will cause the new version of luci to get stuck and deadlock. so to optimize.
    
    luci-app-unblockneteasemusic-mini: refresh servers (#3554)
    
    UnblockNeteaseMusicGo: bump to v0.16
    
    luci-app-unblockneteasemusic: 3in1 newversion (Go,NodeJS,Cloud)
    
    luci-app-unblockmusic: dependence allow mini-version

--- package/lean/luci-app-unblockmusic/root/usr/share/UnblockNeteaseMusic/logcheck.sh
+++ package/lean/luci-app-unblockmusic/root/usr/share/UnblockNeteaseMusic/logcheck.sh
@@ -1,17 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 
 log_max_size="100" #使用KB计算
 log_file="/tmp/unblockmusic.log"
 
 while true
 do
-  sleep 30s
-  icount=`busybox ps -w | grep app.js |grep -v grep| wc -l`
-  if [ $icount -ne 2 ] ;then 
-    /etc/init.d/unblockmusic restart 
-  fi
+	sleep 10s
+	icount=`busybox ps -w | grep UnblockNeteaseMusic/app.js |grep -v grep| wc -l`
+	if [ $icount -ne 1 ] ;then 
+		/etc/init.d/unblockmusic restart 
+	fi
 	(( log_size = "$(ls -l "${log_file}" | awk -F ' ' '{print $5}')" / "1024" ))
 	(( "${log_size}" >= "${log_max_size}" )) && echo "" > /tmp/unblockmusic.log
 	sleep 10m
+	icount=`busybox ps -w | grep UnblockNeteaseMusic |grep -v grep`
+	[ -z "$icount" ] && /etc/init.d/unblockmusic restart 
+	log_size=$(expr $(ls -l $log_file | awk '{print $5}') / 1024)
+	[ $log_size -ge $log_max_size ] && echo "$(date -R) # Start UnblockNeteaseMusic" >/tmp/unblockmusic.log
 done
-
